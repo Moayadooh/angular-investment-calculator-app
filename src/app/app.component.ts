4d2910f60f30f7d31fa9dcc7397707a713aebc70
Moayadooh <moayadsalim97@gmail.com>
2025-06-30 18:18:45 +0400

    using signals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 import { HeaderComponent } from './header/header.component';
 import { UserInputComponent } from "./user-input/user-input.component";
@@ -12,14 +12,14 @@ import { InvestmentResultsComponent } from "./investment-results/investment-resu
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  resultsData?: {
+  resultsData = signal<{
       year: number;
       interest: number;
       valueEndOfYear: number;
       annualInvestment: number;
       totalInterest: number;
       totalAmountInvested: number;
-  }[]
+  }[] | undefined>(undefined);
 
   onCalculateInvestmentResults(data: InvestmentInput) {
     const { initialInvestment, duration, expectedReturn, annualInvestment } = data;
@@ -41,7 +41,7 @@ export class AppComponent {
         totalAmountInvested: initialInvestment + annualInvestment * year,
       });
     }
-    
-    this.resultsData = annualData;
+
+    this.resultsData.set(annualData);
   }
 }
